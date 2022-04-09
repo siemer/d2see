3e3124b6171ba1ae22dc8c766759cd23c6dbfe35
Robert Siemer <Robert.Siemer-github.com@backsla.sh>
2022-04-09 04:52:58 +0200

    custom_container_test.py: remove own Gdk.Window
    * start using parent Gdk.Window
    * simplify code

--- custom_container_test.py
+++ custom_container_test.py
@@ -7,11 +7,10 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
 
 class TestContainer(Gtk.Container):
-    __gtype_name__ = "TestContainer"
-
     def __init__(self, *args, **kwargs):
-        self.children = []
         super().__init__()
+        self.children = []
+        self.set_has_window(False)
 
     def do_add(self, widget):
         self.children.append(widget)
@@ -45,54 +44,23 @@ class TestContainer(Gtk.Container):
 
     def do_size_allocate(self, allocation):
         print("do_size_allocate()")
-        child_allocation = Gdk.Rectangle()
-
         self.set_allocation(allocation)
-        print('get_has_window()', self.get_has_window())
 
-        if self.get_has_window():
-            if self.get_realized():
-                self.get_window().move_resize(allocation.x, allocation.y, allocation.width, allocation.height)
+        child_allocation = Gdk.Rectangle()
+        child_allocation.x = allocation.x
+        child_allocation.y = allocation.y + 30
 
         for widget in self.children:
             if widget.get_visible():
                 min_size, nat_size = widget.get_preferred_size()
-
-                child_allocation.x = 0
-                child_allocation.y = 0
-
-                if not widget.get_has_window():
-                    child_allocation.x = child_allocation.x + allocation.x
-                    child_allocation.y = child_allocation.x + allocation.x
-
                 child_allocation.width = min_size.width
                 child_allocation.height = min_size.height
 
                 widget.size_allocate(child_allocation)
-
-    def do_realize(self):
-        print("do_realize()")
-        allocation = self.get_allocation()
-        
-        attr = Gdk.WindowAttr()
-        attr.window_type = Gdk.WindowType.CHILD
-        attr.x = allocation.x
-        attr.y = allocation.y
-        attr.width = allocation.width
-        attr.height = allocation.height
-        attr.visual = self.get_visual()
-        attr.event_mask = self.get_events() | Gdk.EventMask.EXPOSURE_MASK
-        
-        WAT = Gdk.WindowAttributesType
-        mask = WAT.X | WAT.Y | WAT.VISUAL
-        
-        window = Gdk.Window(self.get_parent_window(), attr, mask);
-        window.set_decorations(0)
-        self.set_window(window)
-        self.register_window(window)
-        self.set_realized(True)
+                child_allocation.x += child_allocation.width - 10
 
     def do_draw(self, cr):
+        print('draw()')
         allocation = self.get_allocation()
         Gtk.render_background(self.get_style_context(), cr, 0, 0, allocation.width, allocation.height)
 
@@ -103,24 +71,18 @@ class TestWindow(Gtk.Window):
     __gtype_name__ = "TestWindow"
     def __init__(self):
         Gtk.Window.__init__(self, title="GTK3 PyGObject Custom Container Test")
-
-        label = Gtk.Label(label = "Text1")
-
-        print('test_window.get_has_window():', self.get_has_window())
         self.area = TestContainer()
-        print('test_container.get_has_window():', self.area.get_has_window())
-        self.area.add(label)
+        self.area.add(Gtk.Label(label = "Text1"))
         self.area.add(Gtk.Label(label='Text2'))
         self.add(self.area)
-        print('test_container.get_has_window() [added]:', self.area.get_has_window())
         self.show_all()
 
     def _on_quit(self, widget, event):
         Gtk.main_quit()
 
 
-MainWindow = TestWindow()
-MainWindow.connect("delete-event", MainWindow._on_quit)
-MainWindow.show_all()
-print(MainWindow.area.child_type())
-Gtk.main()
+if __name__ == '__main__':
+    MainWindow = TestWindow()
+    MainWindow.connect("delete-event", MainWindow._on_quit)
+    MainWindow.show_all()
+    Gtk.main()
