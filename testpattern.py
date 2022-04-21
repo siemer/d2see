f11d877b762ca3fee4b80cac06b07aed0eb7b3d6
Robert Siemer <Robert.Siemer-github.com@backsla.sh>
2022-04-21 17:37:18 +0200

    incorporate custom_container_test.py in testpattern.py
    * minimal styling is in place now

--- testpattern.py
+++ testpattern.py
@@ -1,58 +1,99 @@
 #!/usr/bin/python3
 
-from curses.textpad import rectangle
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
 
-class TestPattern(Gtk.Window):
-    def __init__(self):
+class TestPattern(Gtk.Bin):
+    def __init__(self, *args, **kwargs):
         super().__init__()
-        self.box = box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        self.da = da = Gtk.DrawingArea()
-        self.label = label = Gtk.Label(label='Hello!')
-        self.bb = bb = Gtk.ButtonBox()
-        self.b_close = b_close = Gtk.Button(label='Close')
-        self.add(box)
-        box.pack_start(da, True, True, 0)
-        box.add(label)
-        box.add(bb)
-        bb.add(b_close)
-        da.connect('draw', self.draw)
-        style_provider = Gtk.CssProvider()
-        style_provider.load_from_data(b'''
-        * { color: #ff0077;
-        background-color: #ffffff }
-        ''')
-        style_context = self.get_style_context().add_provider(style_provider,
-            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
-        self.show_all()
 
-        self.inner_label = Gtk.Label(label='I’m inside!')
+    def do_size_allocate(self, allocation):
+        print("do_size_allocate()")
+        self.set_allocation(allocation)
+        self.margin = 10
+        self.steps4x = 20
+        self.steps4y = 8
+        calc_step = lambda total, steps: (total - 2*self.margin) // steps
+        self.xstep = calc_step(allocation.width, self.steps4x)
+        self.ystep = calc_step(allocation.height, self.steps4y)
 
-    def draw(self, widget, context):
-        # il = self.inner_label
-        # rectangle = Gdk.Rectangle()
-        # rectangle.height = 100
-        # rectangle.width = 100
-        # il.size_allocate(rectangle)
-        # il.do_draw(self, context)
-        # return
-        c = context
-        w, h = widget.get_allocated_width(), widget.get_allocated_height()
+        r = Gdk.Rectangle()
+        r.x = 2 * self.margin + self.xstep
+        r.y = 2 * self.margin + self.ystep
+        r.width = allocation.width - 2 * r.x
+        r.height = allocation.height - 2 * r.y
+        self.get_child().size_allocate(r)
+
+    def draw_square(self, c, x0, y0, xstep, ystep, color):
+        c.set_source_rgb(color, color, color)
+        x1 = x0 + xstep
+        y1 = y0 + ystep
+        c.move_to(x0, y0)
+        c.line_to(x1, y0)
+        c.line_to(x1, y1)
+        c.line_to(x0, y1)
+        c.fill()
+
+    def draw_segment(self, c, x0, y0, xstep, ystep, xdir, ydir, start_color):
+        assert 0 in (xdir, ydir)
+        steps = self.steps4x if xdir else self.steps4y
+        color_step = 1 / (steps - 1)
+        if start_color == 1:
+            color_step *= -1
+        # as we draw overlapping segments, I can skip the last one...
+        for i in range(steps - 1):
+            self.draw_square(c, x0, y0, xstep, ystep, start_color+color_step*i)
+            x0 += xdir * xstep
+            y0 += ydir * ystep
+
+    def draw_segments(self, c, w, h):
+        x0 = y0 = self.margin
+        def draw(x, y, xdir, ydir, start_color):
+            self.draw_segment(c, x, y, self.xstep, self.ystep, xdir, ydir, start_color)
+        x1 = x0 + (self.steps4x - 1) * self.xstep
+        y1 = y0 + (self.steps4y - 1) * self.ystep
+        draw(x0, y0, 1, 0, 0)
+        draw(x1, y0, 0, 1, 1)
+        draw(x1, y1, -1, 0, 0)
+        draw(x0, y1, 0, -1, 1)
+
+    def do_draw(self, c):
+        allocation = self.get_allocation()
+        w, h = allocation.width, allocation.height
+        print('draw():', w, h)
+        Gtk.render_background(self.get_style_context(), c, 0, 0, w, h)
+
         c.set_source_rgb(1, 1, 1)
         c.paint()
-        step = (w - 20) / 10
-        for i in range(10):
-            startx = 10 + i*step
-            endx = startx + step
-            color = (1/9) * i
-            c.set_source_rgb(color, color, color)
-            c.move_to(startx, 10)
-            c.line_to(endx, 10)
-            c.line_to(endx, 100)
-            c.line_to(startx, 100)
-            c.fill()
+        self.draw_segments(c, w, h)
+        self.propagate_draw(self.get_child(), c)
 
-w = TestPattern()
-Gtk.main()
+
+class PatternWindow(Gtk.Window):
+    def __init__(self):
+        super().__init__(title='d2see test pattern')
+        main = TestPattern()
+        box = Gtk.VBox()
+        label = Gtk.Label(label='Hello!')
+        bb = Gtk.ButtonBox()
+        b_close = Gtk.Button(label='Close')
+        self.add(main)
+        main.add(box)
+        box.pack_start(label, True, True, 0)
+        box.add(bb)
+        bb.add(b_close)
+        style_provider = Gtk.CssProvider()
+        style_provider.load_from_data(b'''
+            * { color: #ff0077;
+            background-color: #ffffff }
+        ''')
+        self.get_style_context().add_provider(style_provider,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+
+        b_close.connect('clicked', lambda w: self.hide())
+
+if __name__ == '__main__':
+    w = PatternWindow()
+    w.show_all()
+    Gtk.main()
