9ac21c0f42705344d39c142fc5efe41158ff24e5
Robert Siemer <Robert.Siemer-github.com@backsla.sh>
2022-04-21 17:31:32 +0200

    let custom_container_test.py draw grey test pattern
    * inherit from Gtk.Bin instead of Gtk.Container
      * seems it does not need `.add()` method etc.
    * add proper routines to draw test pattern

--- custom_container_test.py
+++ custom_container_test.py
@@ -1,88 +1,84 @@
 #!/usr/bin/python3
 
-import sys
-
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
 
-class TestContainer(Gtk.Container):
+class TestPattern(Gtk.Bin):
     def __init__(self, *args, **kwargs):
         super().__init__()
-        self.children = []
-        self.set_has_window(False)
-
-    def do_add(self, widget):
-        self.children.append(widget)
-        widget.set_parent(self)
-
-    def do_forall(self, include_internals, callback, *callback_parameters):
-        print(f"do_forall({include_internals}, {callback}, {callback_parameters})) self = {id(self):x}")
-        for widget in self.children:
-            callback(widget, *callback_parameters)
-
-    def do_get_request_mode(self):
-        print("do_get_request_mode()")
-        return(Gtk.SizeRequestMode.CONSTANT_SIZE)
-
-    def do_get_preferred_height(self):
-        print("do_get_preferred_height()")
-        result = (50, 50)
-        return(result)
-
-    def do_get_preferred_width(self):
-        print("do_get_preferred_width()")
-        min_width = 0
-        nat_width = 0
-
-        for widget in self.children:
-            child_min_width, child_nat_width = widget.get_preferred_width()
-            min_width += child_min_width
-            nat_width += child_nat_width
-
-        return (min_width, nat_width)
 
     def do_size_allocate(self, allocation):
         print("do_size_allocate()")
         self.set_allocation(allocation)
+        self.margin = 10
+        self.steps4x = 20
+        self.steps4y = 8
+        calc_step = lambda total, steps: (total - 2*self.margin) // steps
+        self.xstep = calc_step(allocation.width, self.steps4x)
+        self.ystep = calc_step(allocation.height, self.steps4y)
 
-        child_allocation = Gdk.Rectangle()
-        child_allocation.x = allocation.x
-        child_allocation.y = allocation.y + 30
+        r = Gdk.Rectangle()
+        r.x = 2 * self.margin + self.xstep
+        r.y = 2 * self.margin + self.ystep
+        r.width = allocation.width - 2 * r.x
+        r.height = allocation.height - 2 * r.y
+        self.get_child().size_allocate(r)
 
-        for widget in self.children:
-            if widget.get_visible():
-                min_size, nat_size = widget.get_preferred_size()
-                child_allocation.width = min_size.width
-                child_allocation.height = min_size.height
+    def draw_square(self, c, x0, y0, xstep, ystep, color):
+        c.set_source_rgb(color, color, color)
+        x1 = x0 + xstep
+        y1 = y0 + ystep
+        c.move_to(x0, y0)
+        c.line_to(x1, y0)
+        c.line_to(x1, y1)
+        c.line_to(x0, y1)
+        c.fill()
 
-                widget.size_allocate(child_allocation)
-                child_allocation.x += child_allocation.width - 10
+    def draw_segment(self, c, x0, y0, xstep, ystep, xdir, ydir, start_color):
+        assert 0 in (xdir, ydir)
+        steps = self.steps4x if xdir else self.steps4y
+        color_step = 1 / (steps - 1)
+        if start_color == 1:
+            color_step *= -1
+        # as we draw overlapping segments, I can skip the last one...
+        for i in range(steps - 1):
+            self.draw_square(c, x0, y0, xstep, ystep, start_color+color_step*i)
+            x0 += xdir * xstep
+            y0 += ydir * ystep
 
-    def do_draw(self, cr):
-        print('draw()')
+    def draw_segments(self, c, w, h):
+        x0 = y0 = self.margin
+        def draw(x, y, xdir, ydir, start_color):
+            self.draw_segment(c, x, y, self.xstep, self.ystep, xdir, ydir, start_color)
+        x1 = x0 + (self.steps4x - 1) * self.xstep
+        y1 = y0 + (self.steps4y - 1) * self.ystep
+        draw(x0, y0, 1, 0, 0)
+        draw(x1, y0, 0, 1, 1)
+        draw(x1, y1, -1, 0, 0)
+        draw(x0, y1, 0, -1, 1)
+
+    def do_draw(self, c):
         allocation = self.get_allocation()
-        Gtk.render_background(self.get_style_context(), cr, 0, 0, allocation.width, allocation.height)
+        w, h = allocation.width, allocation.height
+        print('draw():', w, h)
+        Gtk.render_background(self.get_style_context(), c, 0, 0, w, h)
 
-        for widget in self.children:
-            self.propagate_draw(widget, cr)
+        c.set_source_rgb(1, 1, 1)
+        c.paint()
+        self.draw_segments(c, w, h)
+        self.propagate_draw(self.get_child(), c)
 
 class TestWindow(Gtk.Window):
-    __gtype_name__ = "TestWindow"
     def __init__(self):
-        Gtk.Window.__init__(self, title="GTK3 PyGObject Custom Container Test")
-        self.area = TestContainer()
+        super().__init__(title="GTK3 PyGObject Custom Container Test")
+        self.area = TestPattern()
         self.area.add(Gtk.Label(label = "Text1"))
-        self.area.add(Gtk.Label(label='Text2'))
         self.add(self.area)
-        self.show_all()
-
-    def _on_quit(self, widget, event):
-        Gtk.main_quit()
 
 
 if __name__ == '__main__':
     MainWindow = TestWindow()
-    MainWindow.connect("delete-event", MainWindow._on_quit)
+    MainWindow.connect("delete-event", Gtk.main_quit)
     MainWindow.show_all()
     Gtk.main()
